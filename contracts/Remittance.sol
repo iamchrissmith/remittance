0dd1ee4e4609f87499e10023a3fa9dad5ad1e3a0
Chris Smith <42@chrisryansmith.com>
2017-08-21 21:49:32 -0600

    test that the remittance struct is setup and retreiveable

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -2,17 +2,38 @@ pragma solidity ^0.4.6;
 
 contract Remittance {
   struct remitTransaction {
-    address funder;
-    address exchange;
     uint amount;
     bytes32 combinedPassword;
+    uint deadline;
   }
 
   address public owner;
 
-  remitTransaction[] public remitTransactions;
+  mapping(address => remitTransaction) public remitTransactions;
 
   function Remittance() {
     owner = msg.sender;
+  }
+
+  modifier onlyMe() {
+    require( msg.sender == owner );
+    _;
+  }
+
+  function createRemittance(bytes32 pwHash1, bytes32 pwHash2, address exchange, uint _deadline)
+    public
+    onlyMe()
+    payable
+    returns (bool success)
+  {
+    if(msg.value == 0) revert();
+    if(remitTransactions[exchange].amount != 0) revert();
+
+    remitTransaction memory newRemit;
+    newRemit.amount = msg.value;
+    newRemit.combinedPassword = keccak256(pwHash1, pwHash2);
+    newRemit.deadline = block.number + _deadline;
+    remitTransactions[exchange] = newRemit;
+    return true;
   }
 }
--- test/Remittance-test.js
+++ test/Remittance-test.js
@@ -3,7 +3,8 @@ const Remittance = artifacts.require('./Remittance.sol');
 contract('Remittance', (accounts) => {
   const alice = accounts[0],
         carol = accounts[1];
-  let contract;
+  let contract,
+      expectedDeadline;
 
   beforeEach( () => {
     return Remittance.new({from: alice})
@@ -19,13 +20,39 @@ contract('Remittance', (accounts) => {
       });
   });
 
-  context('when Alice sends a Remittance with an amount, recipient and combined password', () => {
-    xit('it should create a Remittance record with the balance and exchange address', () => {});
-    xit('it should store the passwords as secure hashes', () => {});
+
+
+  context('when Alice sends a Remittance with an amount, recipient and passwords', () => {
+    const PASSWORD1 = web3.toHex('abc123');
+    const HASHED_PASSWORD1 = web3.sha3(PASSWORD1, { encoding: 'hex' });
+    const PASSWORD2 = web3.toHex('123abc');
+    const HASHED_PASSWORD2 = web3.sha3(PASSWORD2, { encoding: 'hex' });
+
+    it('it should create a Remittance record with the balance, deadline and exchange address', () => {
+      return contract.createRemittance(HASHED_PASSWORD1, HASHED_PASSWORD2, carol, 10, {from: alice, value:10})
+        .then( (txn) => {
+          expectedDeadline = web3.eth.blockNumber + 10;
+          return contract.remitTransactions(carol, {from:alice});
+        })
+        .then( (remit1) => {
+          assert.equal(remit1[0].toString(10), 10, "Remit Amount is incorrect");
+          expectedCombinedPassword = web3.sha3(HASHED_PASSWORD1 + HASHED_PASSWORD2.slice(2), { encoding: 'hex' });
+          assert.equal(remit1[1], expectedCombinedPassword, "Password Hash is incorrect");
+          assert.equal(remit1[2].toString(10), expectedDeadline, "Remit Deadline is incorrect");
+        });
+    });
+    it('only the contract owner can createRemittance()', () => {
+      return contract.createRemittance(HASHED_PASSWORD1, HASHED_PASSWORD2, carol, 10, {from: carol, value:10})
+        .then( (txn) => {
+          assert.isFalse(txn, "Should not allow non-owners to createRemittance");
+        });
+    });
+    xit('once a remittance is created for an address it cannot create another', () => {});
+
+    describe('.retreiveFunds()', () => {
+      xit('it should not send the funds when one of the passwords is wrong', () => {});
+      xit('it should send the funds when both of the passwords are right', () => {});
+    });
   });
 
-  describe('.retreiveFunds()', () => {
-    xit('it should not send the funds when one of the passwords is wrong', () => {});
-    xit('it should send the funds when both of the passwords are right', () => {});
-  });
 });
